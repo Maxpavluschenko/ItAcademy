f4b03daa17f2f692dae27a5edd1d60f1cb6a3662
Maksym182 <max.pavluschenko@gmail.com>
2018-07-10 16:41:17 +0000

    modified file script.js and images

--- public/script/script.js
+++ public/script/script.js
@@ -1,34 +1,34 @@
+(function () {
+	$(document).ready(function () {
+		$(".owl-carousel").owlCarousel({
+			items: 1,
+			loop: true,
+			dots: false,
+			nav: true,
+			autoplay: true,
+			autoplayHoverPause: true,
+			autoplaySpeed: 1500,
+			smartSpeed: 1500
+		});
+		$("#topnav a").on("click", "a", function (event) {
+			event.preventDefault();
+			var id = $(this).attr('href'),
+				top = $(id).offset().top;
+			$('body,html').animate({
+				scrollTop: top
+			}, 1000);
+		});
+	});
 
-$(document).ready(function owlCarousel(){
-    $(".owl-carousel").owlCarousel({
-      items: 1,
-      loop: true,
-      dots:false,
-      nav: true,
-      autoplay:true,
-      autoplayHoverPause:true,
-      autoplaySpeed:1500,
-      smartSpeed:1500
-    });
-    
-  });
+	document.getElementById('menu').addEventListener('click', hamburgerMenu);
 
-$(document).ready(function animate() {
-    $("#topnav").on("click", "a", function (event) {
-        event.preventDefault();
-        var id = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({ scrollTop: top }, 1000);
-    });
-});
+	function hamburgerMenu() {
+		var x = document.getElementById('topnav');
 
-menu.onclick = function hamburgerMenu() {
-    var x = document.getElementById('topnav');
-
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
+		if (x.className === "topnav") {
+			x.className += " responsive";
+		} else {
+			x.className = "topnav";
+		}
+	}
+})();
